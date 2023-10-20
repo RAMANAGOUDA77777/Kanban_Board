4dd8e45f856ffd768733c2e9e0dd56a26dbb00e3
RAMANAGOUDA77777 <ramanagouda857@gmail.com>
2023-10-20 12:44:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Kanban from "./Components/Kanban";
+
+function App() {
+    return (
+        <div className="App">
+         <Kanban/>
+        </div>
+    );
+}
+
+export default App;
